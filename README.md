d81744ae1ac781f08b5818dc6a21f5f65d63fa5a
Sckathach <sckathach@hackademint.org>
2024-02-18 01:55:56 +0100

    Preprocessing embeddings to use a database storage

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 - Simple RAG: https://medium.com/@thakermadhav/build-your-own-rag-with-mistral-7b-and-langchain-97d0c92fa146
 - Conversational RAG: https://github.com/madhavthaker1/llm/blob/main/rag/conversational_rag.ipynb
 - LangChain doc: https://python.langchain.com/docs/modules/data_connection/document_loaders/file_directory
+- Anyscale: https://www.anyscale.com/blog/a-comprehensive-guide-for-building-rag-based-llm-applications-part-1
+
+## Deployment using Ray?
+- Ray on Kubernetes: https://docs.ray.io/en/latest/cluster/kubernetes/index.html
 
 ## Troubleshooting
 - CUDA out of memory error &rarr; reduced chunk size from 600 to 300 and added 
--- rag.py
+++ rag.py
@@ -1,8 +1,9 @@
 from langchain_community.document_loaders import UnstructuredMarkdownLoader, DirectoryLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, Document
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import nest_asyncio
+from typing import List, Dict, Any
 
 nest_asyncio.apply()
 
@@ -10,13 +11,40 @@ loader = DirectoryLoader('Kubernetes', glob="**/*.md", loader_cls=UnstructuredMa
 docs = loader.load()
 
 assert len(docs) == 5
+chunk_size = 300
+chunk_overlap = 50
 
-text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0)
-chunked_documents = text_splitter.split_documents(docs)
-db = FAISS.from_documents(chunked_documents,
-                          HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
-
-retriever = db.as_retriever(
-    search_type="similarity",
-    search_kwargs={'k': 4}
+text_splitter = CharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap,
 )
+chunked_documents = text_splitter.split_documents(docs)
+# db = FAISS.from_documents(chunked_documents,
+#                           HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2'))
+#
+# retriever = db.as_retriever(
+#     search_type="similarity",
+#     search_kwargs={'k': 4}
+# )
+
+
+class EmbedChunks:
+    def __init__(self, model_name: str):
+        self.embedding_model = HuggingFaceEmbeddings(
+            model_name=model_name
+        )
+
+    def __call__(self, batch: List[Document]) -> List[Dict[str, Any]]:
+        embeddings = self.embedding_model.embed_documents([x.page_content for x in batch])
+        return [
+            {
+                "text": x.page_content,
+                "source": x.metadata["source"],
+                "embeddings": y
+            } for x, y in zip(batch, embeddings)
+        ]
+
+
+embedder = EmbedChunks(model_name="sentence-transformers/all-mpnet-base-v2")
+r = embedder(chunked_documents)
+print(r[:10])
